87e9e25d0227089146cfbe750f0274aadad00f08
kubirthan <kubirthan7991@gmail.com>
2024-08-20 16:24:39 +0530

    connect database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const app = require("./app");
 const dotenv = require("dotenv");
-const path = require('path')
+const path = require('path');
+const connectDatabase = require("./config/database");
 
-dotenv.config({ path: path.join(__dirname,"config/config.env")});
+dotenv.config({path: path.join(__dirname,"config/config.env")});
+
+connectDatabase()
 
 app.listen(process.env.PORT, () => {
   console.log(
