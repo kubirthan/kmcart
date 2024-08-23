e92c2a74a9bc614dacf2a8784627582b25c29cd0
kubirthan <kubirthan7991@gmail.com>
2024-08-23 21:40:36 +0530

    update Product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -34,3 +34,25 @@ exports.getSingleProduct = async (req, res, next) => {
         product
     })
 }
+
+//Update Product
+exports.updateProduct = async(req,res,next) => {
+    let product = await Product.findById(req.params.id)
+
+    if(!product){
+        return res.status(404).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+
+   product =  await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    })
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,10 +1,12 @@
 const express = require('express')
-const { getProducts, newProduct, getSingleProduct } = require('../controllers/productController')
+const { getProducts, newProduct, getSingleProduct, updateProduct } = require('../controllers/productController')
 const router = express.Router()
 
 router.route('/products').get(getProducts)
 router.route('/product/new').post(newProduct)
-router.route('/product/:id').get(getSingleProduct)
+router.route('/product/:id')
+                            .get(getSingleProduct)
+                            .put(updateProduct)
 
 
 
