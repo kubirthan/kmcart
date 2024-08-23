6ab3667db43420e544813d0273c42f5f69b6c09f
kubirthan <kubirthan7991@gmail.com>
2024-08-23 13:11:13 +0530

    Get All Products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,12 +1,16 @@
 const Product = require('../models/productModel')
 
-exports.getProducts = (req, res, next)=>{
-    res.status(200).json({
-        success: true,
-        message: "This route will show the all products in database"
-    })
+//get Products - /api/v1/products
+exports.getProducts = async(req, res, next)=>{
+  const products =   await Product.find()
+  res.status(200).json({
+    success: true,
+    count: products.length,
+    products
+  })
 }
 
+//Create product - /api/v1/product/new
 exports.newProduct = async (req, res, next) => {
     const product = await Product.create(req.body)
     res.status(201).json({
