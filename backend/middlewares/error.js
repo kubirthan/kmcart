ea6e9b13fab4398e00728356d84ecc37be9430a9
kubirthan <kubirthan7991@gmail.com>
2024-08-24 11:18:35 +0530

    Environment based Errors

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -1,12 +1,22 @@
-const { stack } = require("../app")
+
 
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500
 
-    res.status(err.statusCode).json({
-        success: false,
-        message: err.message,
-        stack: err.stack
-    })
+    if(process.env.NODE_ENV == 'development') {
+        res.status(err.statusCode).json({
+            success: false,
+            message: err.message,
+            stack: err.stack
+        })
+    }
+
+    if(process.env.NODE_ENV == 'production'){
+        res.status(err.statusCode).json({
+            success: false,
+            message: err.message
+        })
+    }
+    
 }
 
