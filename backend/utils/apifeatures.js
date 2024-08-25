3a7ee82e27bee2ffbdf065ee3b3db7f91d3be488
kubirthan <kubirthan7991@gmail.com>
2024-08-25 13:47:58 +0530

    Price filter

--- backend/utils/apifeatures.js
+++ backend/utils/apifeatures.js
@@ -5,7 +5,7 @@ class APIFeatures {
     }
 
     search(){
-       let keyword =  this.query.keyword ? {
+       let keyword =  this.queryStr.keyword ? {
             name: {
                 $regex: this.queryStr.keyword,
                 $options: 'i'
@@ -23,11 +23,13 @@ class APIFeatures {
         const removeFields = ['keyword', 'limit', 'page']
         removeFields.forEach(field => delete queryStrCopy[field])
 
+        let queryStr = JSON.stringify(queryStrCopy)
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)/g, match => `$${match}`)
 
-        this.query.find(queryStrCopy)
+        
+        this.query.find(JSON.parse(queryStr))
+        
         return this
-        
-        
     }
 }
 
