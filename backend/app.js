67170398e34bd43bb86c5894005c37ff60cef719
kubirthan <kubirthan7991@gmail.com>
2024-08-26 10:41:50 +0530

    Creating register api

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,12 @@ const app = express()
 const errorMiddleware = require('./middlewares/error')
 
 const products = require('./routes/product')
+const auth = require('./routes/auth')
 
 app.use(express.json())
 
 app.use('/api/v1/',products)
+app.use('/api/v1/', auth)
 
 app.use(errorMiddleware)
 
