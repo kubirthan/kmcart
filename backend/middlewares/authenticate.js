1fef32a82010ca29d1dac3e95acb7a082bb9b701
kubirthan <kubirthan7991@gmail.com>
2024-08-28 20:05:58 +0530

    Authorize User Roles

--- backend/middlewares/authenticate.js
+++ backend/middlewares/authenticate.js
@@ -14,3 +14,12 @@ exports.isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
     req.user = await User.findById(decoded.id)
     next()
 })
+
+exports.authorizeRoles = (...roles) => {
+    return (req, res, next) => {
+        if(!roles.includes(req.user.role)){
+            return next(new ErrorHandler(`Role ${req.user.role} is not allowed`, 401))
+        }
+        next()
+    }
+}
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productController')
 const router = express.Router()
-const {isAuthenticatedUser} = require('../middlewares/authenticate')
+const {isAuthenticatedUser, authorizeRoles} = require('../middlewares/authenticate')
 
 router.route('/products').get(isAuthenticatedUser,getProducts)
-router.route('/product/new').post(newProduct)
+router.route('/product/new').post(isAuthenticatedUser,authorizeRoles('admin'),newProduct)
 router.route('/product/:id')
                             .get(getSingleProduct)
                             .put(updateProduct)
